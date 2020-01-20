cea63e28a886d97428c63248867ff330983c8f01
Sinziany <57525663+Sinziany@users.noreply.github.com>
2020-01-20 16:49:48 +0200

    Add files via upload

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 'MARIUS31',
+    name: 'Marius Giurca',
+    username: 'Bret',
+    email: 'Marius@april.biz'
+  },
+  {
+    id: 'Sinziana32',
+    name: 'Sinziana Flueras',
+    username: 'Antonette',
+    email: 'Sinziana@melissa.tv'
+  },
+  {
+    id: 'Paula24',
+    name: 'Paula Giurca',
+    username: 'Samantha',
+    email: 'Paula@yesenia.net'
+  },
+  {
+    id: 'Mirela39',
+    name: 'Mirela Barlea',
+    username: 'Karianne',
+    email: 'Mirela@kory.org'
+  },
+  {
+    id: 'Dragos8',
+    name: 'Dragos Barlea',
+    username: 'Kamren',
+    email: 'Dragos@annie.ca'
+  },
+  {
+    id: 'Florin41',
+    name: 'Florin Flueras',
+    username: 'Leopoldo_Corkery',
+    email: 'Florin@jasper.info'
+  },
+  {
+    id: 7,
+    name: 'Kurtis Weissnat',
+    username: 'Elwyn.Skiles',
+    email: 'Telly.Hoeger@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Nicholas Runolfsdottir V',
+    username: 'Maxime_Nienow',
+    email: 'Sherwood@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'Glenna Reichert',
+    username: 'Delphine',
+    email: 'Chaim_McDermott@dana.io'
+  },
+  {
+    id: 10,
+    name: 'Clementina DuBuque',
+    username: 'Moriah.Stanton',
+    email: 'Rey.Padberg@karina.biz'
+  }
+];
